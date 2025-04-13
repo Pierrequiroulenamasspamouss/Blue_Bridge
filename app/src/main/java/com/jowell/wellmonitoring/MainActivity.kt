7b5b485d7f0a1bc645e8e27ae598e3f9d12baf36
Pierre <pierresluse@gmail.com>
2025-04-13 11:35:22 +0200

    test2

--- app/src/main/java/com/jowell/wellmonitoring/MainActivity.kt
+++ app/src/main/java/com/jowell/wellmonitoring/MainActivity.kt
@@ -89,6 +89,7 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                     )
+                    //test
                 }
             }
         }
