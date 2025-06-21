b1628b8ece4f0c59c4b2927da691b754a6973ea7
Pierrequiroulenamasspamouss <pierresluse@gmail.com>
2025-06-22 01:20:32 +0200

    Fixed things

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@
 // ======================
 // Configuration Constants
 // ======================
-const appLatestVersion = '0.1.4.2   ';
+const appLatestVersion = '0.1.4.3   ';
 const serverVersion = '1.0.2';
 const isDev = process.env.NODE_ENV === 'development' || true;
 const PORT = process.env.PORT || 80;
