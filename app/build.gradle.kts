40719b42e45ed83ae97da96e3ac5724decb5096d
Pierrequiroulenamasspamouss <pierresluse@gmail.com>
2025-06-27 00:58:41 +0200

    t

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.4.5"
+        versionName = "0.1.4.6-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
