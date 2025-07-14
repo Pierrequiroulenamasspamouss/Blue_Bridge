6acd33ba49074a621ca771c8ea87d985576ed143
Pierre Sluse <pierresluse@gmail.com>
2025-07-14 18:01:10 +0200

    v0.1.4.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.4.9"
+        versionName = "0.1.4.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
